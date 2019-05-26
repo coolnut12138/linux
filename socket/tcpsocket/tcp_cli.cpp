372b9925db906e6139d52024facb94cb183f9a26
coolnut12138 <1426345106@qq.com>
2019-05-25 22:21:31 -0700

    基于tcp协议的客户端与服务端网络通信

---
+++ socket/tcpsocket/tcp_cli.cpp
@@ -0,0 +1,42 @@
+/*通过封装的TcpSocket类实现网络通信客户端*/
+/*
+ * 1、创建套接字
+ * 2、向服务端发起连接请求
+ * 3、收发数据
+ * 4、关闭套接字*/
+
+#include "tcpsocket.hpp"
+#include <signal.h>
+
+void sigcb(int no) {
+    std::cout << "recv signo:" << no << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc != 3) {
+	std::cout << "./tcp_cli ip port" << std::endl;
+	return -1;
+    }
+    signal(SIGPIPE, sigcb);
+    std::string ip = argv[1];
+    uint16_t port = atoi(argv[2]);
+
+    TcpSocket sock;
+    CHECK_RET(sock.Socket());
+    CHECK_RET(sock.Connect(ip, port));
+
+    while(1){
+	std::string buf;
+	std::cout << "client say:";
+	fflush(stdout);
+	std::cin >> buf;
+	sock.Send(buf);
+
+	buf.clear();
+	sock.Recv(buf);
+	std::cout << "server say:" << buf << std::endl;
+    }
+    sock.Close();
+    return 0;
+}
