5e943d9a5b4f4ce75122280758c02c2d3213f238
coolnut12138 <1426345106@qq.com>
2019-08-12 10:29:59 +0800

    practice

--- thread/mythread/BuyAndSaleModel.cpp
+++ thread/mythread/BuyAndSaleModel.cpp
@@ -1,128 +1,136 @@
 /*实现生产者与消费者模型*/
-
 #include <iostream>
 #include <queue>
-#include <string>
 #include <pthread.h>
-#include <unistd.h>
 using namespace std;
 
-class BlockQueue{
-public:
-    BlockQueue(int cap = 10)
-	:_capacity(cap)
-    {
-	pthread_mutex_init(&_mutex, NULL);
-	pthread_cond_init(&_productor, NULL);
-	pthread_cond_init(&_customer, NULL);
-    }
-    ~BlockQueue()
-    {
-	pthread_mutex_destroy(&_mutex);
-	pthread_cond_destroy(&_productor);
-	pthread_cond_destroy(&_customer);
-    }
+class TaskQueue
+{
+    public:
+	TaskQueue(int cap = 10)
+	    :_capacity(cap)
+	{
+	    pthread_mutex_init(&_mutex, NULL);
+	    pthread_cond_init(&_productor, NULL);
+	    pthread_cond_init(&_customer, NULL);
+	}
+	
+	~TaskQueue()
+	{
+	    pthread_mutex_destroy(&_mutex);
+	    pthread_cond_destroy(&_productor);
+	    pthread_cond_destroy(&_customer);
+	}
 
-    void QueuePush(int val)
-    {
-	QueueLock();
-	while(QueueIsFull()){
-	    ProductorWait();
+	void QueuePush(int val)
+	{
+	    QueueLock();
+	    while(QueueIsFull()){
+		ProductorWait();
+	    }
+	    _queue.push(val);
+	    QueueUnLock();
+	    CustomerWakeUp();
 	}
-	_queue.push(val);
-	QueueUnlock();
-	CustomerWakeUp();
-    }
-    void QueuePop(int *val)
-    {
-	QueueLock();
-	while(QueueIsEmpty()){
-	    CustomerWait();
+
+	void QueuePop(int *val)
+	{
+	    QueueLock();
+	    while(QueueIsEmpty()){
+		CustomerWait();
+	    }
+	    *val = _queue.front();
+	    _queue.pop();
+	    QueueUnLock();
+	    ProductorWakeUp();
 	}
-	*val = _queue.front();
-	_queue.pop();
-	QueueUnlock();
-	ProductorWakeUp();
-    }
-private:
-    queue<int> _queue;
-    int _capacity;
-    pthread_mutex_t _mutex;
-    pthread_cond_t _productor;
-    pthread_cond_t _customer;
-private:
-    void QueueLock(){
-	pthread_mutex_lock(&_mutex);
-    }
-    void QueueUnlock(){
-	pthread_mutex_unlock(&_mutex);
-    }
-    void CustomerWait(){
-	pthread_cond_wait(&_customer, &_mutex);
-    }
-    void CustomerWakeUp(){
-	pthread_cond_signal(&_customer);
-    }
-    void ProductorWait(){
-	pthread_cond_wait(&_productor, &_mutex);
-    }
-    void ProductorWakeUp(){
-	pthread_cond_signal(&_productor);
-    }
-    bool QueueIsEmpty(){
-	return _queue.empty();
-    }
-    bool QueueIsFull(){
-	return (_queue.size() == _capacity);
-    }
+    private:
+	queue<int> _queue;
+	int _capacity;
+	pthread_mutex_t _mutex;
+	pthread_cond_t _productor;
+	pthread_cond_t _customer; 
+    private:
+	void QueueLock()
+	{
+	    pthread_mutex_lock(&_mutex);
+	}
+	void QueueUnLock()
+	{
+	    pthread_mutex_unlock(&_mutex);
+	}
+	void CustomerWait()
+	{
+	    pthread_cond_wait(&_customer, &_mutex);
+	}
+	void CustomerWakeUp()
+	{
+	    pthread_cond_signal(&_customer);
+	}
+	void ProductorWait()
+	{
+	    pthread_cond_wait(&_productor, &_mutex);
+	}
+	void ProductorWakeUp()
+	{
+	    pthread_cond_signal(&_productor);
+	}
+	bool QueueIsFull()
+	{
+	    return _queue.size() == _capacity;
+	}
+	bool QueueIsEmpty()
+	{
+	    return _queue.empty();
+	}
 };
 
-void *customer(void *arg)
+void* productor(void* arg)
 {
-    BlockQueue *q = (BlockQueue*)arg;
+    TaskQueue* q = (TaskQueue*)arg;
+    int i = 0; 
     while(1){
-	int data;
-	q->QueuePop(&data);
-	cout << "get data:" << data << endl;
+	q->QueuePush(i);
+	cout << "producting data -------------- :" << i << endl;
+	i++;
     }
     return NULL;
 }
 
-void *productor(void *arg)
+void* customer(void* arg)
 {
-    BlockQueue *q = (BlockQueue*)arg;
-    int i = 0;
+    TaskQueue* q = (TaskQueue*)arg;
     while(1){
-	q->QueuePush(i);
-	cout << "---put data:" << i << endl;
-	i++;
+	int data;
+	q->QueuePop(&data);
+	cout << "Geting data&&&&&&&&&&&&&&&&&&&&&&&&&&& :" << data << endl;
     }
     return NULL;
 }
 
 int main()
 {
-    BlockQueue q;
-    pthread_t c_tid[4], p_tid[4];
+    TaskQueue tq;
+    pthread_t c_tid[4];
+    pthread_t p_tid[4];
     for(int i = 0; i < 4; i++){
-	if(pthread_create(&p_tid[i], NULL, productor, (void*)&q) != 0){
-	    cout << "create thread error" << endl;
+	int ret = pthread_create(&p_tid[i], NULL, productor, (void*)&tq);
+	if(ret != 0){
+	    cout << "pthread_create error" << endl;
 	    return -1;
 	}
     }
     for(int i = 0; i < 4; i++){
-	if(pthread_create(&c_tid[i], NULL, customer, (void*)&q) != 0){
-	    cout << "create thread error" << endl;
+	int ret = pthread_create(&c_tid[i], NULL, customer, (void*)&tq);
+	if(ret != 0){
+	    cout << "pthread_create error" << endl;
 	    return -1;
 	}
     }
 
     for(int i = 0; i < 4; i++){
-	pthread_join(c_tid[i], NULL);    
-    }
-    for(int i = 0; i < 4; i++){
-	pthread_join(p_tid[i], NULL);    
+	pthread_join(c_tid[i], NULL);
+	pthread_join(p_tid[i], NULL);
     }
     return 0;
 }
-
